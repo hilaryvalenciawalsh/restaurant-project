fdad538c7de10db968eab0b62f5f3bcca1bdb34a
Jonny K <Kempj2.jk@gmail.com>
2020-10-07 11:22:12 -0700

    adding in a some sudo code

---
+++ index.js
@@ -0,0 +1,15 @@
+//all npm install
+
+//backend
+    //setup an express server
+    //make sure to include data parse
+    //Port special heroku thingyyy PORT = process.env.PORT || 3000
+    
+    //making handle requests
+    //routes:
+        //get index.html with /
+        //get reserve.html with /reserve
+        //get tables.thlm with /tables
+
+        //post handled from /reserve to add data to JSON
+        //
